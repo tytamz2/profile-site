269ccd72881c9d2fbd18900e93715de12d11849f
Tyler Tam <tytam1326@gmail.com>
2024-08-12 11:13:16 -0700

    Added Resume Page

--- frontend/src/componets/Resume/Resume.js
+++ frontend/src/componets/Resume/Resume.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import Pdf from './Resume-2';
 import { Document, Page } from 'react-pdf';
 import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
@@ -8,9 +9,27 @@ import Col from 'react-bootstrap/Col';
 
 function Resume(){
 
+    const Download = () =>{
+        const Url = Pdf
+        const link = document.createElement("a");
+        link.href = Url
+        link.download = Pdf
+        document.body.appendChild(link);
+        link.click();
+        document.body.removeChild(link);
+    }
     return(
         <div>
             <h1>Resume</h1>
+            <Document file={Pdf}>
+                <Page pageNumber={1}/>
+            </Document>
+
+            <center>
+                <Button onClick={Download}>
+                    DownLoad Resume
+                </Button>
+            </center>
         </div>
     )
 }
