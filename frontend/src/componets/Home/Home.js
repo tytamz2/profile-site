104ddbbe9cbbf634e2147afd449588dbe660047f
Tyler Tam <tytam1326@gmail.com>
2024-08-26 10:19:26 -0700

    Working On homepage

--- frontend/src/componets/Home/Home.js
+++ frontend/src/componets/Home/Home.js
@@ -4,6 +4,7 @@ import Modal from 'react-bootstrap/Modal';
 import Stack from 'react-bootstrap/Stack';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import Container from 'react-bootstrap/esm/Container';
 
 function Home(){
 
@@ -11,7 +12,24 @@ function Home(){
 
     return(
         <div>
-            <h2>Profile</h2>
+            <h1>Profile</h1>
+
+            <Container>
+                <Col className='photo'>
+                    {/* Photo Here */}
+                </Col>
+                <Col>
+                    <Row>
+                        <h2>Tyler Tam</h2>
+                        <h3>Computer Programmer</h3>
+                    </Row>
+                    <Row className='About'>
+                        <p>
+                            I enjoy playing online games, taking walks outside and watching sports
+                        </p>
+                    </Row>
+                </Col>
+            </Container>
         </div>
 
     )
