b508729103fada7a33def3a9d7a23c82b839ba3a
Tyler Tam <tytam1326@gmail.com>
2025-01-27 11:04:33 -0800

    working on project

--- frontend/src/componets/Projects/Projects.js
+++ frontend/src/componets/Projects/Projects.js
@@ -3,6 +3,7 @@ import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Container from 'react-bootstrap/esm/Container';
 import Card from 'react-bootstrap/Card';
+import Accordion from 'react-bootstrap/Accordion';
 import '../../css/Projects.css'
 
 function Projects(){
@@ -14,22 +15,31 @@ function Projects(){
 
             <Container className='prj' fluid>
                 <Row>
-                    <Col className='col1'>
-                    <Card style={{ width: '20rem' }}>
+                    <Col xs lg="4">
+                    <Card style={{ width: '20rem' }} className='col1'>
                 <Card.Body>
                 <Card.Title>Activity Tracker</Card.Title>
                 <Card.Subtitle className="mb-2 text-muted">Team Project</Card.Subtitle>
                 <Card.Text>
                     Activity Tracker applications allows users
                     to create and view tasks, users are able filter
-                    and time thier tasks
+                    and time thier tasks, Great for keeping track of 
+                    current tasks needed to be done
                 </Card.Text>
+                <Accordion>
+                <Accordion.Item eventKey="0">
+              <Accordion.Header>Frameworks</Accordion.Header>
+              <Accordion.Body>
+                React, nextJs, SQL, Bootstrap
+              </Accordion.Body>
+              </Accordion.Item>
+                </Accordion>
                 <Card.Link href="#">Site Link</Card.Link>
                 </Card.Body>
                  </Card>
                     </Col>
-                    <Col className='col2'>
-                    <Card style={{ width: '20rem' }}>
+                    <Col >
+                    <Card style={{ width: '20rem' }} className='col2'>
                     <Card.Body>
                         <Card.Title>Chat Appplication</Card.Title>
                         <Card.Subtitle className="mb-2 text-muted">Team Project</Card.Subtitle>
@@ -37,14 +47,22 @@ function Projects(){
                         Simple Chat application. users are able to create an account
                         to chat with other users, allows users to edit thier profile
                         </Card.Text>
+                        <Accordion>
+                            <Accordion.Item eventKey="0">
+                            <Accordion.Header>Frameworks</Accordion.Header>
+                            <Accordion.Body>
+                            Java,React, MongoDB, etc
+                            </Accordion.Body>
+                            </Accordion.Item>
+                </Accordion>
                         <Card.Link href="#">Card Link</Card.Link>
                     </Card.Body>
                     </Card>
                     </Col>
                 </Row>
                 <Row>
-                    <Col className='col3'>
-                    <Card style={{ width: '20rem' }}>
+                    <Col >
+                    <Card style={{ width: '20rem' }} className='col3'>
                     <Card.Body>
                         <Card.Title>Profle Website</Card.Title>
                         <Card.Subtitle className="mb-2 text-muted">Indivdual Project</Card.Subtitle>
@@ -52,6 +70,14 @@ function Projects(){
                         Basic Website to display my personal profile and provide contact info
                         and resume
                         </Card.Text>
+                        <Accordion>
+                            <Accordion.Item eventKey="0">
+                            <Accordion.Header>Frameworks</Accordion.Header>
+                            <Accordion.Body>
+                            JS,React, etc
+                            </Accordion.Body>
+                            </Accordion.Item>
+                        </Accordion>
                         <Card.Link href="#">Repo Link</Card.Link>
                     </Card.Body>
                     </Card>
